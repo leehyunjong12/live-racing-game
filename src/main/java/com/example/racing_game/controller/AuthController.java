b87068eedeadf87774b9718cfba88b67e1149979
hj_git <alclssha4665@gmail.com>
2025-11-21 15:00:05 +0900

    fix: 로그인시 세션 저장 수정

--- src/main/java/com/example/racing_game/controller/AuthController.java
+++ src/main/java/com/example/racing_game/controller/AuthController.java
@@ -12,12 +12,15 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
+import org.springframework.security.web.context.SecurityContextRepository;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -26,6 +29,8 @@ public class AuthController {
 
     private final AuthService authService;
     private final AuthenticationManager authenticationManager;
+    private final SecurityContextRepository securityContextRepository =
+            new HttpSessionSecurityContextRepository();
 
     @PostMapping("/register")
     public ResponseEntity<String> register(@RequestBody RegisterRequest request) {
@@ -37,11 +42,14 @@ public class AuthController {
         }
     }
     @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody LoginRequest request) {
+    public ResponseEntity<String> login(@RequestBody LoginRequest request,HttpServletRequest servletRequest,
+                                        HttpServletResponse servletResponse) {
         try {
             Authentication authentication = authService.login(request.getUsername(), request.getPassword());
-            SecurityContextHolder.getContext().setAuthentication(authentication);
-
+            SecurityContext context = SecurityContextHolder.createEmptyContext();
+            context.setAuthentication(authentication);
+            SecurityContextHolder.setContext(context);
+            securityContextRepository.saveContext(context, servletRequest, servletResponse);
             return ResponseEntity.ok("로그인 성공: " + request.getUsername());
 
         } catch (AuthenticationException e) {
